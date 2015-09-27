51d54fb95e5424b5695c992649db350390d4bef2
grensund <grensund@gmail.com>
2015-09-27 16:37:22 +0200

    Här kommer bonusläxan
    I lagarbete med LRF Media Corp.

--- day1/homework/email_writer.py
+++ day1/homework/email_writer.py
@@ -6,18 +6,31 @@ Gör namnet mejlkompatibelt och skriv ut en fullständig e-postadress.
 
 
 def emailify(name, domain):
-    """
-     Koden ska göra följande:
-     1) Ersätta mellanslag med punkter i namnet.
-     2) Göra alla tecken till små bokstäver
-     3) Ersätta specialtecken (å,ä,ö,é) med tecken som funkar
-     i en e-postadress.
-     4) Printa den kompletta adressen! 
-    """
-    # Skriv din kod här! 
+	text = name.lower()\
+		.replace("å", "a")\
+		.replace("ä", "a")\
+		.replace("ö", "o")\
+		.replace("Å", "a")\
+		.replace("Ä", "a")\
+		.replace("Ö", "o")\
+		.replace("é", "e")\
+		.replace("É", "e")\
+		.replace(" ",".")
+
+	email = "%s@%s" % (text, domain.lower())
+	print(email)
+
+# VARIANT MED STEG FÖR STEG I VARIABLER
+#    small_name = "%s@%s" % (name.lower(), domain.lower())
+#    english_name = small_name.replace("é", "e").replace("å", "a").replace("ä", "a").replace("ö", "o").replace("Å", "a").replace("Ä", "a").replace("Ö", "o")
+#    no_dot_name = english_name.replace(" ", ".")
+#    print(no_dot_name)
+
 
 emailify("Annie Lööf", "riksdagen.se")
 emailify("David Lång", "riksdagen.se")
 emailify("Emma Nohrén", "riksdagen.se")
 emailify("阿部慎太郎", "asahi.co.jp")
 emailify("Östen Rubin", "dn.se")
+emailify("Örjan Stor-Knobben Larsson", "yahoo.nk")
+emailify("Åke Grävare-Échelon", "gmail.com")
