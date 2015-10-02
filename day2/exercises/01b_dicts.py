d7f143c9b3741ed08667b169275db99bd30801ca
grensund <grensund@gmail.com>
2015-10-02 16:30:54 +0200

    utförd

--- day2/exercises/01b_dicts.py
+++ day2/exercises/01b_dicts.py
@@ -14,19 +14,23 @@ solna = {
 }
 
 
-
-
 """ Hur många procentenheter har arbetslösheten stigit i Stockholm?
-""" 
-# Skriv kod här!
+"""
+sthlm_diff = (stockholm["unemployment_2014"]) - (stockholm["unemployment_2009"]) # Skriv kod här!
+print "Arbetslösheten i Stockholm har stigit med %s procentenheter." % (sthlm_diff)
+
+# print (stockholm[unemployment_2014 - unemployment_2009]) # Skriv kod här!
 
 
 """ Hur många procentenheter har arbetslösheten stigit i Solna?
-""" 
-# Skriv kod här!
+"""
+solna_diff = (solna["unemployment_2014"]) - (solna["unemployment_2009"]) # Skriv kod här!
+print "Arbetslösheten i Solna har stigit med %s procentenheter." % (solna_diff)
+
+# print (solna[unemployment_2014 - unemployment_2009]) # Skriv kod här!
 
 
 """ Hur mycket högra arbetslöshet hade Stockholm än Solna 2014?
 """
-# Skriv kod här!
-
+diff = (stockholm["unemployment_2014"]) - (solna["unemployment_2014"]) # Skriv kod här!
+print "Arbetslösheten i Stockholm var %d procent större 2014." % (diff / solna["unemployment_2014"] * 100)
