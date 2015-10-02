7c9fe370f83fa2b8a4ca5303671a099121a4d8b4
grensund <grensund@gmail.com>
2015-10-02 16:16:40 +0200

    utförd

--- day2/exercises/03a_return.py
+++ day2/exercises/03a_return.py
@@ -1,19 +1,24 @@
 # -*- coding: utf-8 -*-
 
-
-""" 
+"""
 ÖVNING
 Gör en funktion som tar en arbetslöshetsprocent och kategoriserar den
 som "låg", "medel" eller "hög" med hjälp av IF-satser.
-Använd IF-satser 
+Använd IF-satser
 """
+
+
 def categorize_unemployment(unemployment):
     if unemployment < 5.0:
         return "låg"
+    elif unemployment < 10.0:
+    	return "medel"
+    else:
+    	return "hög"
 
 
-print( categorize_unemployment(1.2) )
-print( categorize_unemployment(10.3) )
-print( categorize_unemployment(7.8) )
-print( categorize_unemployment(5.6) )
-print( categorize_unemployment(2.1) )
+print(categorize_unemployment(1.2))
+print(categorize_unemployment(10.3))
+print(categorize_unemployment(7.8))
+print(categorize_unemployment(5.6))
+print(categorize_unemployment(2.1))
