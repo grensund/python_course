0fcf3ce41b9a91dab105bac1e81362f1d16f13e6
grensund <grensund@gmail.com>
2015-10-03 18:06:28 +0200

    update med andelar för resp parti

--- day2/homework/01_count_sons.py
+++ day2/homework/01_count_sons.py
@@ -26,47 +26,82 @@ Idé: använd for-loop för varje parti tillsammans med if-sats
 
 """
 
-counter_fp = 0
-counter_c = 0
-counter_kd = 0
-counter_mp = 0
-counter_v = 0
-counter_m = 0
-counter_sd = 0
-counter_s = 0
+counter_fp = 0.0
+counter_c = 0.0
+counter_kd = 0.0
+counter_mp = 0.0
+counter_v = 0.0
+counter_m = 0.0
+counter_sd = 0.0
+counter_s = 0.0
+total_fp = 0.0
+total_c = 0.0
+total_kd = 0.0
+total_mp = 0.0
+total_v = 0.0
+total_m = 0.0
+total_sd = 0.0
+total_s = 0.0
 
-counter_son_names = 0
+counter_son_names = 0.0
 total_number_of_mps = len(data)
 
 # Här har jag använt "or" i if-satsen för att kombinera två stycken villkor.
 for row in data:
-    if "son," in row["name"] or "son ," in row["name"]:
-    	counter_son_names = counter_son_names + 1
 	if "FP" in row["party"]:
-		counter_fp = counter_fp + 1
+		total_fp = total_fp + 1
 	if "SD" in row["party"]:
-		counter_sd = counter_sd + 1
+		total_sd = total_sd + 1
 	if "C" in row["party"]:
-		counter_c = counter_c + 1
+		total_c = total_c + 1
 	if "KD" in row["party"]:
-		counter_kd = counter_kd + 1
+		total_kd = total_kd + 1
 	if "S" in row["party"]:
-		counter_s = counter_s + 1
+		total_s = total_s + 1
 	if "MP" in row["party"]:
-		counter_mp = counter_mp + 1
+		total_mp = total_mp + 1
 	if "V" in row["party"]:
-		counter_v = counter_v + 1
+		total_v = total_v + 1
 	if "M" in row["party"]:
-		counter_m = counter_m + 1
+		total_m = total_m + 1
 	
-# Dubbelräkningen av mp och sd subtraherade från m respektive s.
-print("Av %s ledamöter har %s son-namn" % (total_number_of_mps, counter_son_names))
-print("Antal moderater: %s.\
-	\nAntal liberaler: %s.\
-	\nAntal centerpartister: %s\
-	\nAntal kristdemokrater: %s\
-	\nAntal sossar: %s.\
-	\nAntal miljömuppar: %s\
-	\nAntal vpk:are: %s.\
-	\nAntal sd:are: %s") % (counter_m - counter_mp, counter_fp, counter_c, counter_kd, counter_s - counter_sd, counter_mp, counter_v, counter_sd)
 
+for row in data:
+	if "son," in row["name"] or "son ," in row["name"]:
+		counter_son_names = counter_son_names + 1
+		if "FP" in row["party"]:
+			counter_fp = counter_fp + 1
+		if "SD" in row["party"]:
+			counter_sd = counter_sd + 1
+		if "C" in row["party"]:
+			counter_c = counter_c + 1
+		if "KD" in row["party"]:
+			counter_kd = counter_kd + 1
+		if "S" in row["party"]:
+			counter_s = counter_s + 1
+		if "MP" in row["party"]:
+			counter_mp = counter_mp + 1
+		if "V" in row["party"]:
+			counter_v = counter_v + 1
+		if "M" in row["party"]:
+			counter_m = counter_m + 1
+
+# Dubbelräkningen av mp och sd subtraherade från m respektive s.
+# Använde %d för att slippa decimaler
+print("Av %d ledamöter har %d son-namn" % (total_number_of_mps, counter_son_names))
+print("Antal moderater: %d (%d procent).\
+	\nAntal liberaler: %d (%d procent).\
+	\nAntal centerpartister: %d (%d procent).\
+	\nAntal kristdemokrater: %d (%d procent).\
+	\nAntal sossar: %d (%d procent).\
+	\nAntal miljömuppar: %d (%d procent).\
+	\nAntal vpk:are: %d (%d procent).\
+	\nAntal sd:are: %d (%d procent).")\
+% (counter_m - counter_mp, ((counter_m - counter_mp) / (total_m - total_mp)) * 100, \
+	counter_fp, counter_fp / total_fp * 100, \
+	counter_c, counter_c / total_c * 100, \
+	counter_kd, counter_kd / total_kd * 100, \
+	counter_s - counter_sd, (counter_s - counter_sd) / (total_s - total_sd) * 100, \
+	counter_mp, counter_mp / total_mp * 100, \
+	counter_v, counter_v / total_v * 100, \
+	counter_sd, counter_sd / total_sd * 100)
